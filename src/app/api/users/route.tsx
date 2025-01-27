4a5cc145598a88fd001ace0d92bec5446ecfa730
Omkar <omkar@excitesys.com>
2025-01-27 13:09:19 +0530

    Initial commit

---
+++ src/app/api/users/route.tsx
@@ -0,0 +1,30 @@
+import { dbConnect } from "../../../../lib/dbConnect";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+  });
+
+  const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+//   export async function POST(request : Request) {
+//     try {
+//       await dbConnect(); // Connect to DB only if not connected
+//       const body = await request.json();
+//       const newUser = await User.create(body);
+//       return Response.json({ success: true, data: newUser });
+//     } catch (error:any) {
+//       return Response.json({ success: false, error: error.message }, { status: 500 });
+//     }
+//   }
+
+  export async function GET() {
+    try {
+      await dbConnect(); // Connect to DB only if not connected
+      const users = await User.find({});
+      return Response.json({ success: true, data: users });
+    } catch (error :any) {
+      return Response.json({ success: false, error: error.message }, { status: 500 });
+    }
+  }
